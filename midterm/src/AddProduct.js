f4367b24ac0ef136854f66b33e3a84113e5e842d
amellortizt <138019361+amellortizt@users.noreply.github.com>
2023-10-18 21:07:45 -0400

    First part completed

--- midterm/src/AddProduct.js
+++ midterm/src/AddProduct.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 
 const AddProduct = () => {
     const [formData, setFormData] = useState({
-        productName: '',
-        productPrice: '',
-        productDescription: '',
+        name: '',
+        description: '',
+        category: '',
+        quantity: '',
+        price: '',
     });
 
     const handleInputChange = (e) => {
@@ -20,31 +22,50 @@ const AddProduct = () => {
 
     return (
         <div>
-            <h2>Add Product</h2>
+            <h2>Midterm part 1</h2>
             <form onSubmit={handleSubmit}>
                 <div>
-                    <label>Product Name:</label>
+                    <label>Name</label>
                     <input
                         type="text"
-                        name="productName"
-                        value={formData.productName}
+                        name="name"
+                        value={formData.name}
                         onChange={handleInputChange}
                     />
                 </div>
                 <div>
-                    <label>Product Price:</label>
+                    <label>Description</label>
                     <input
                         type="text"
-                        name="productPrice"
-                        value={formData.productPrice}
+                        name="description"
+                        value={formData.description}
                         onChange={handleInputChange}
                     />
                 </div>
                 <div>
-                    <label>Product Description:</label>
-                    <textarea
-                        name="productDescription"
-                        value={formData.productDescription}
+                    <label>Category</label>
+                    <input
+                        type="text"
+                        name="category"
+                        value={formData.category}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label>Quantity</label>
+                    <input
+                        type="text"
+                        name="quantity"
+                        value={formData.quantity}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label>Price</label>
+                    <input
+                        type="text"
+                        name="price"
+                        value={formData.price}
                         onChange={handleInputChange}
                     />
                 </div>
