8deac146bda63b2479da319694f119f3ce5038ff
amellortizt <138019361+amellortizt@users.noreply.github.com>
2023-10-18 21:28:24 -0400

    Working on the second part

---
+++ midterm/src/SignUpUser.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
+
+const SignUpUser = () => {
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data, null, 2));
+        console.log(data);
+    };
+
+    return (
+        <div>
+            <h2>Midterm part 2</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label>First Name</label>
+                    <Controller
+                        name="firstName"
+                        control={control}
+                        rules={{ required: true }}
+                        render={({ field }) => <input {...field} />}
+                    />
+                    {errors.firstName && <p>This field is required.</p>}
+                </div>
+                <div>
+                    <label>Last Name</label>
+                    <Controller
+                        name="lastName"
+                        control={control}
+                        rules={{ required: true }}
+                        render={({ field }) => <input {...field} />}
+                    />
+                    {errors.lastName && <p>This field is required.</p>}
+                </div>
+                <div>
+                    <label>Username</label>
+                    <Controller
+                        name="userName"
+                        control={control}
+                        rules={{ required: true }}
+                        render={({ field }) => <input {...field} />}
+                    />
+                    {errors.userName && <p>This field is required.</p>}
+                </div>
+                <div>
+                    <label>Email</label>
+                    <Controller
+                        name="email"
+                        control={control}
+                        rules={{
+                            required: true,
+                            pattern: /^\S+@\S+$/i,
+                        }}
+                        render={({ field }) => <input {...field} />}
+                    />
+                    {errors.email && errors.email.type === 'required' && (
+                        <p>This field is required.</p>
+                    )}
+                    {errors.email && errors.email.type === 'pattern' && (
+                        <p>Invalid email format.</p>
+                    )}
+                </div>
+                <div>
+                    <label>Password</label>
+                    <Controller
+                        name="password"
+                        control={control}
+                        rules={{ required: true }}
+                        render={({ field }) => <input type="password" {...field} />}
+                    />
+                    {errors.password && <p>This field is required.</p>}
+                </div>
+                <div>
+                    <label>Confirm Password</label>
+                    <Controller
+                        name="confirmPassword"
+                        control={control}
+                        rules={{
+                            required: true,
+                            validate: (value) =>
+                                value === control.getValues().password ||
+                                'Passwords do not match',
+                        }}
+                        render={({ field }) => <input type="password" {...field} />}
+                    />
+                    {errors.confirmPassword && errors.confirmPassword.type === 'required' && (
+                        <p>Please complete. This field is required!</p>
+                    )}
+                    {errors.confirmPassword && errors.confirmPassword.type === 'validate' && (
+                        <p>{errors.confirmPassword.message}</p>
+                    )}
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default SignUpUser;
--- midterm/src/index.js
+++ midterm/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AddProduct from './AddProduct';
+import SignUpUser from './SignUpUser';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,6 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AddProduct />
+    <SignUpUser />
   </React.StrictMode>
 );
 
